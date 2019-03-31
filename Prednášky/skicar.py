63d1b0722e29bfe7852b7225fd5f25b53cf50a12
Miroslav Hájek <mirkousko@gmail.com>
2019-03-31 14:04:00 +0200

    Pridaj ďalšie útvary - obdĺžnik, elipsu, spolu s klávesovými skratkami

--- Prednášky/skicar.py
+++ Prednášky/skicar.py
@@ -6,7 +6,12 @@ def vytvor_utvar(mys):
 
     klik = not klik
     if klik:
-        utvar = okno.create_line(mys.x, mys.y, mys.x, mys.y)
+        if mod == CIARA:
+            utvar = okno.create_line(mys.x, mys.y, mys.x, mys.y)
+        elif mod == OBDLZNIK:
+            utvar = okno.create_rectangle(mys.x, mys.y, mys.x, mys.y)
+        elif mod == ELIPSA:
+            utvar = okno.create_oval(mys.x, mys.y, mys.x, mys.y)
     else:
         pos = okno.coords(utvar)
         okno.coords(utvar, pos[0], pos[1], mys.x, mys.y)
@@ -18,12 +23,25 @@ def animuj_utvar(mys):
         okno.coords(utvar, pos[0], pos[1], mys.x, mys.y)
 
 
+def zmen_utvar(klavesa):
+    global mod
+
+    if klavesa.char in [CIARA, OBDLZNIK, ELIPSA]:
+        mod = klavesa.char
+
+
 W, H = 800, 500
 klik = False
 utvar = 0
+
+CIARA = 'c'
+OBDLZNIK = 'o'
+ELIPSA = 'e'
+mod = CIARA
 
 okno = tkinter.Canvas(width=W, height=H, bg='white')
 okno.pack()
 okno.bind('<Button-1>', vytvor_utvar)
 okno.bind('<Motion>', animuj_utvar)
+okno.bind_all("<Key>", zmen_utvar)
 okno.mainloop()
