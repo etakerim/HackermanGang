5ff46763f7793c2f09857fcd44eb5b32353fc621
Miroslav Hájek <mirkousko@gmail.com>
2019-03-31 14:11:34 +0200

    Základná paleta farieb prístupná cez klávesy

--- Prednášky/skicar.py
+++ Prednášky/skicar.py
@@ -7,11 +7,11 @@ def vytvor_utvar(mys):
     klik = not klik
     if klik:
         if mod == CIARA:
-            utvar = okno.create_line(mys.x, mys.y, mys.x, mys.y)
+            utvar = okno.create_line(mys.x, mys.y, mys.x, mys.y, fill=farba)
         elif mod == OBDLZNIK:
-            utvar = okno.create_rectangle(mys.x, mys.y, mys.x, mys.y)
+            utvar = okno.create_rectangle(mys.x, mys.y, mys.x, mys.y, outline=farba)
         elif mod == ELIPSA:
-            utvar = okno.create_oval(mys.x, mys.y, mys.x, mys.y)
+            utvar = okno.create_oval(mys.x, mys.y, mys.x, mys.y, outline=farba)
     else:
         pos = okno.coords(utvar)
         okno.coords(utvar, pos[0], pos[1], mys.x, mys.y)
@@ -24,10 +24,13 @@ def animuj_utvar(mys):
 
 
 def zmen_utvar(klavesa):
-    global mod
+    global mod, farba
 
     if klavesa.char in [CIARA, OBDLZNIK, ELIPSA]:
         mod = klavesa.char
+
+    elif klavesa.char in ['0', '1', '2', '3']:
+        farba = paleta[int(klavesa.char)]
 
 
 W, H = 800, 500
@@ -38,6 +41,9 @@ CIARA = 'c'
 OBDLZNIK = 'o'
 ELIPSA = 'e'
 mod = CIARA
+
+paleta = ['black', 'red', 'green', 'blue']
+farba = paleta[0]
 
 okno = tkinter.Canvas(width=W, height=H, bg='white')
 okno.pack()
